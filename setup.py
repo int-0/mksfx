d597e4560adaebfeae79b9ba1b8bf59b858ece08
int-0 <tobias.deb@gmail.com>
2016-08-07 18:42:09 +0200

    First release

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mksfx',
+      version='1.0',
+      description='Make python self-extracting file',
+      author='Tobias Diaz',
+      author_email='tobias.deb@gmail.com',
+      url='https://github.com/int-0/mksfx/',
+      scripts=['mksfx']
+  )
+
+      
